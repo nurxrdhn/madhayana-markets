37bf379c47cb818cec69d86ae3d89689b5d66f61
nurxrdhn <ramadhannur148@gmail.com>
2026-06-13 15:34:59 +0700

    Delete api directory

--- api/dana-finish.js
+++
@@ -1,4 +0,0 @@
-export default async function handler(req, res) {
-  if (req.method !== 'POST') return res.status(405).json({ responseMessage: 'Method not allowed' });
-  return res.status(200).json({ responseCode: '2000000', responseMessage: 'SUCCESS' });
-}
