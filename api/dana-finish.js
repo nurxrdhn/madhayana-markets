0c716f02b1d1221b7b2fe3fb003778dd59750f15
nurxrdhn <ramadhannur148@gmail.com>
2026-06-13 15:36:15 +0700

    Add files via upload

---
+++ api/dana-finish.js
@@ -0,0 +1,1 @@
+export default function handler(req,res){res.status(200).json({responseCode:'2000000',responseMessage:'SUCCESS'})}
---
+++ api/dana/generate-qris.js
@@ -0,0 +1,1 @@
+export default function handler(req,res){res.status(200).json({mode:'mock',status:'PENDING',qrContent:'',responseMessage:'QRIS statis aktif. Hubungkan DANA credential untuk production.'})}
